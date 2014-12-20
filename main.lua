456919859cde6946f6def7b678d14d47c8c8a04c
flyingfisch <flyingfisch@toppagedesign.com>
2014-12-20 16:43:22 -0500

    got moving lines YAY!

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+-- [[ LÖVE2D CALLBACKS ]]--
 function love.load()
 	player = {}
 	player.x, player.y = 50, 50
@@ -7,22 +8,31 @@ function love.load()
 	objective = {}
 	objective.x, objective.y = 75, 75
 	objective.color = {50, 50, 200}
+
+	-- lines = {{x, y, vertical, direction, color}}
+	lines = {{x=100, y=100, vertical=false, direction=1, color={50, 50, 200}}, {x=150, y=150, vertical=true, direction=1, color={50, 50, 200}}}
+	lineLength = 20
+	lineSpeed = 100
 end
 
 function love.update(dt)
 	keyHandler(dt)
+	updateLines(lines, dt)
 end
 
 function love.draw()
 	drawPlayer(player.x, player.y)
 	drawObjective(objective.x, objective.y)
+
+	drawLines(lines)
 end
 
--- custom functions
+
+
+--[[ FUNCTIONS ]]--
 function keyHandler(dt)
 	local speed = player.speed * dt
 
-	-- keys
 	if love.keyboard.isDown("left") then
 		player.x = player.x - speed
 	elseif love.keyboard.isDown("right") then
@@ -40,6 +50,7 @@ function keyHandler(dt)
 	end
 end
 
+-- draw functions
 function drawPlayer(x, y)
 	love.graphics.setColor(player.color[1], player.color[2], player.color[3])
 	love.graphics.circle("fill", x, y, 10, 25)
@@ -48,4 +59,32 @@ end
 function drawObjective(x, y)
 	love.graphics.setColor(objective.color[1], objective.color[2], objective.color[3])
 	love.graphics.circle("fill", objective.x, objective.y, 10, 25)
+end
+
+function drawLines(lines)
+	for key, line in pairs(lines) do
+		love.graphics.setColor(line.color[1], line.color[2], line.color[3])
+
+		if line.vertical then
+			love.graphics.line(line.x, line.y, line.x, line.y + lineLength)
+		else
+			love.graphics.line(line.x, line.y, line.x + lineLength, line.y)
+		end
+	end
+end
+
+-- update functions
+function updateLines(lines, dt)
+	local speed = lineSpeed * dt
+	for key, line in pairs(lines) do
+		if line.vertical then
+			if line.y + lineLength > love.graphics.getHeight() then
+				line.direction = -1
+			elseif line.y < 0 then
+				line.direction = 1
+			end
+
+			line.y = line.y + (speed * line.direction)
+		end
+	end
 end
